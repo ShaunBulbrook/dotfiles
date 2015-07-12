983bae8ff00e16c3d1671807496414572389c4bc
Dan Hughes <danhughes.dh+github@gmail.com>
2015-07-12 14:58:55 +0100

    Modify Send to allow multiple file sending

--- misc/rsync-to-haumea.sh
+++ misc/rsync-to-haumea.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 source /home/pi/.dotfiles/bash/settings.sh
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Usage: $0 filename"
 	exit 1
 fi
 
-rsync \
-	-avz \
-	$1 \
-	192.168.1.23:/home/pi/inbox \
-	--remove-source-files \
-	--progress
+for file in "$@" ; do
+    rsync \
+    	-avz \
+    	$file \
+    	192.168.1.23:/home/pi/inbox \
+    	--remove-source-files \
+    	--progress
 
-if [[ $? -eq 0 ]]; then
-    TITLE="Sent to Haumea"
-else
-    TITLE="Rsync failed"
-fi
+    if [[ $? -eq 0 ]]; then
+        TITLE="Sent to Haumea"
+    else
+        TITLE="Rsync failed"
+    fi
 
-curl \
--u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
-	-d type=note \
-	-d title="$TITLE" \
-	-d body="$1" \
-> /dev/null
+    curl -sS \
+    -u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
+    	-d type=note \
+    	-d title="$TITLE" \
+    	-d body="$file" \
+    > /dev/null
+done
