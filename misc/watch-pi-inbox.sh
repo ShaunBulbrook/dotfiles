7b466d884d7fc490829489383f694d4f38555844
Dan Hughes <danhughes.dh+github@gmail.com>
2015-10-29 21:19:48 +0000

    Quote files to allow for spaces in filenames

--- misc/watch-pi-inbox.sh
+++ misc/watch-pi-inbox.sh
@@ -5,15 +5,15 @@ source /home/pi/.dotfiles/bash/settings.sh
 shopt -s nullglob
 
 for FILE in /home/pi/inbox/*; do
-	sudo chown root: $FILE
-	sudo mv $FILE /media/sync/Dan\ -\ Torrents/inbox
+		sudo chown root: "$FILE"
+		sudo mv "$FILE" /media/sync/Dan\ -\ Torrents/inbox
 
-	if [[ $? -eq 0 ]]; then
-		curl \
-		-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
-		-d type=note \
-		-d title="File added to Sync" \
-		-d body="$(basename $FILE)" \
-		> /dev/null
-	fi
+		if [[ $? -eq 0 ]]; then
+				curl \
+				-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
+				-d type=note \
+				-d title="File added to Sync" \
+				-d body="$(basename '$FILE')" \
+				> /dev/null
+		fi
 done
