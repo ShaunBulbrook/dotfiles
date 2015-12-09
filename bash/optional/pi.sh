113704ae2783c13bd9b935bcd5da81b8d713a12f
Dan Hughes <danhughes.dh+github@gmail.com>
2015-12-09 23:30:59 +0000

    Add updateall alias for updating and rebooting

--- bash/optional/pi.sh
+++ bash/optional/pi.sh
@@ -1,2 +1,3 @@
 alias temp='/opt/vc/bin/vcgencmd measure_temp'
 alias off='sudo poweroff'
+alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremote -y && sudo apt-get clean && sudo reboot'
