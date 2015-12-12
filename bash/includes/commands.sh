6e75003671d2bf2b471b7a14aa4d0cc408ee5612
Dan Hughes <danhughes.dh+github@gmail.com>
2015-12-12 12:39:53 +0000

    Fix updateall command typo, make global

--- bash/includes/commands.sh
+++ bash/includes/commands.sh
@@ -1,4 +1,3 @@
 alias get='apt-get install'
-alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get clean'
-
+alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove -y && sudo apt-get clean'
 alias ssh-forget='ssh-keygen -f "~/.ssh/known_hosts" -R '
--- bash/optional/pi.sh
+++ bash/optional/pi.sh
@@ -1,3 +1,2 @@
 alias temp='/opt/vc/bin/vcgencmd measure_temp'
 alias off='sudo poweroff'
-alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremote -y && sudo apt-get clean && sudo reboot'
