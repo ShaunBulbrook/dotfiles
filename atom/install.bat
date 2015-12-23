055bf3a925bc3d0d29deaaa5b36ec776461b1a4d
Dan Hughes <danhughes.dh+github@gmail.com>
2015-12-23 13:01:34 +0000

    Link Atom files into dot-prefixed folder

--- atom/install.bat
+++ atom/install.bat
@@ -5,7 +5,7 @@ set files=config.cson,keymap.cson,snippets.cson,styles.less
 
 for %%f in (%files%) do (
 	del %HOMEPATH%\.atom\%%f
-	mklink %HOMEPATH%\.atom\%%f %HOMEPATH%\dotfiles\atom\%%f
+	mklink %HOMEPATH%\.atom\%%f %HOMEPATH%\.dotfiles\atom\%%f
 )
 
 pause
