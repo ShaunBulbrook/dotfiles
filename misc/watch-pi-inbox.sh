5f8fe196b1dbc42b2bbe1a44efe390449e1f4f25
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-01 20:22:56 +0000

    Update raspi watcher to take CLI args

--- misc/watch-pi-inbox.sh
+++ misc/watch-pi-inbox.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-source /home/pi/.dotfiles/bash/settings.sh
 
-# Expand * to null if none found
+if [ "$#" -lt 2 ]; then
+	echo "Usage: $0 <input folder> <output folder>"
+	exit 1
+fi
+
+source /home/pi/.dotfiles/bash/settings.sh
 shopt -s nullglob
 
-for FILE in /home/pi/inbox/*; do
-		sudo chown root: "$FILE"
-		sudo mv "$FILE" /media/sync/Dan\ -\ Torrents/inbox
+input=$1
+output=$2
 
-		if [[ $? -eq 0 ]]; then
-				curl \
-				-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
-				-d type=note \
-				-d title="File added to Sync" \
-				-d body="$(basename '$FILE')" \
-				> /dev/null
-		fi
+for FILE in "$input"/*; do
+	sudo chown root: "$FILE"
+	sudo mv "$FILE" "$output"
+	filename=$(basename "$FILE")
+
+	if [[ $? -eq 0 ]]; then
+		curl -sS \
+		-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
+		-d type=note \
+		-d title="File added to Sync" \
+		-d body="$filename" \
+		> /dev/null
+	fi
 done
