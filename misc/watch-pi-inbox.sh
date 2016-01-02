3a71e513f45b3bd6b0bef71bcddf226ec7f5d5d2
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-02 15:13:13 +0000

    Wait for file to finish syncing before moving

--- misc/watch-pi-inbox.sh
+++ misc/watch-pi-inbox.sh
@@ -12,16 +12,18 @@ input=$1
 output=$2
 
 for FILE in "$input"/*; do
-	sudo chown root: "$FILE"
-	sudo mv "$FILE" "$output"
-	filename=$(basename "$FILE")
+	if [ ${FILE: -4} != ".bts" ]; then
+		sudo chown root: "$FILE"
+		sudo mv "$FILE" "$output"
+		filename=$(basename "$FILE")
 
-	if [[ $? -eq 0 ]]; then
-		curl -sS \
-		-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
-		-d type=note \
-		-d title="File added to Sync" \
-		-d body="$filename" \
-		> /dev/null
+		if [[ $? -eq 0 ]]; then
+			curl -sS \
+			-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
+			-d type=note \
+			-d title="File added to Sync" \
+			-d body="$filename" \
+			> /dev/null
+		fi
 	fi
 done
