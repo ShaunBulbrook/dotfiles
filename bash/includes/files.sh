5f5a12450a0dea4c2f8e353d22a9b202e8ac522a
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-19 23:11:10 +0000

    Clear console on reconfig

--- bash/includes/files.sh
+++ bash/includes/files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-alias reconfig='source ~/.bashrc'
+alias reconfig='source ~/.bashrc; clear'
 alias exify='chmod u+x'
 alias gpgify='gpg --encrypt --recipient $GPGKEY --trust-model always --armor'
 alias tmp='cd $(mktemp -d)'
