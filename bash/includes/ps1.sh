ad32111168c31f0ddf2524ef361a8bc7e47f7fd1
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-20 16:01:56 +0000

    Make PS1 prompt colouring easier to understand

--- bash/includes/ps1.sh
+++ bash/includes/ps1.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-PS1='\
-\n\
-\[\033[${USERNAMECOLOUR}\]\
-\h\
-\[\033[1;00m\] \
-\[\033[${PWDCOLOUR}\]\
-\w\
-\[\033[1;00m\]\
-\n\
-$ '
+black='\e[0;30m'
+red='\e[0;31m'
+green='\e[0;32m'
+brown='\e[0;33m'
+purple='\e[0;35m'
+blue='\e[0;34m'
+cyan='\e[0;36m'
+lightgray='\e[0;37m'
+darkgray='\e[1;30m'
+lightred='\e[1;31m'
+lightgreen='\e[1;32m'
+yellow='\e[1;33m'
+lightblue='\e[1;34m'
+lightpurple='\e[1;35m'
+lightcyan='\e[1;36m'
+white='\e[1;37m'
+
+usrcol="\e[${USERNAMECOLOUR}"
+pwdcol="\e[${PWDCOLOUR}"
+r='\e[0m'
+
+PS1="\n${usrcol}\h${r} ${pwdcol}\w${r}\n${white}λ${r} "
