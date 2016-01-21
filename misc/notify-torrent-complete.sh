8ff18251225eb1fce2fee0f48889365531b29cf9
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-21 00:33:44 +0000

    Update torrent notifier from Transmission to Deluge

--- misc/notify-torrent-complete.sh
+++ misc/notify-torrent-complete.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 source /home/pi/.dotfiles/bash/settings.sh
 
+id=$1
+name=$2
+path=$3
+
 curl \
-	-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
-	-d type=note \
-	-d title="Torrent complete" \
-	-d body="$TR_TORRENT_NAME" \
-	> /dev/null
+		-u "$PUSHBULLET_API_KEY:" https://api.pushbullet.com/v2/pushes \
+		-d type=note \
+		-d title="Torrent complete" \
+		-d body="$name" \
+		> /dev/null
 exit 0
