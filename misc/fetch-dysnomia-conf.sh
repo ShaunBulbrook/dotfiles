d205b7d362e3023de5c74888066a1b5716e3f539
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-25 22:50:34 +0000

    Fix up some formatting and redirect output

--- misc/fetch-dysnomia-conf.sh
+++ misc/fetch-dysnomia-conf.sh
@@ -5,11 +5,11 @@ src=$1
 
 curl -sSL $src | gpg --passphrase-file $dir/passphrase.txt -d > $dir/sync.new.conf
 
-diff $dir/sync.conf $dir/sync.new.conf 
+diff $dir/sync.conf $dir/sync.new.conf >/dev/null 2>&1
 status=$?
 
 if [ $status -ne 0 ]; then
 	mv $dir/sync.new.conf $dir/sync.conf
 	chown pi: $dir/sync.conf
-	killall btsync && $dir/btsync --config $dir/sync.conf	
+	killall btsync && $dir/btsync --config $dir/sync.conf
 fi
