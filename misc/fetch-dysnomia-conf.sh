4c69222bac93a074d88d6503a4e7e73308d7a985
Dan Hughes <danhughes.dh+github@gmail.com>
2016-01-25 23:23:53 +0000

    Remove new sync file if not different to old one

--- misc/fetch-dysnomia-conf.sh
+++ misc/fetch-dysnomia-conf.sh
@@ -3,7 +3,7 @@
 dir='/home/pi/.btsync'
 src=$1
 
-curl -sSL $src | gpg --passphrase-file $dir/passphrase.txt -d > $dir/sync.new.conf
+curl -sSL $src | gpg --batch --passphrase-file $dir/passphrase.txt -d > $dir/sync.new.conf
 
 diff $dir/sync.conf $dir/sync.new.conf >/dev/null 2>&1
 status=$?
@@ -11,5 +11,8 @@ status=$?
 if [ $status -ne 0 ]; then
 	mv $dir/sync.new.conf $dir/sync.conf
 	chown pi: $dir/sync.conf
-	killall btsync && $dir/btsync --config $dir/sync.conf
+	killall btsync
+	$dir/btsync --config $dir/sync.conf
+else
+	rm $dir/sync.new.conf
 fi
