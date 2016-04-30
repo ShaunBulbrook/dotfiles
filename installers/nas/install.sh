d774d9bf706abae5681491e1d6da8f245975e33c
Dan Hughes <danhughes.dh+github@gmail.com>
2016-04-30 22:49:29 +0100

    Point Raspberry Pi NAS mount to shared Videos folder

--- installers/nas/install.sh
+++ installers/nas/install.sh
@@ -3,4 +3,4 @@
 sudo apt-get install cifs-utils
 sudo mkdir -p /media/nas
 touch /home/pi/.smbcredentials
-echo "//192.168.0.102/pi /media/nas cifs credentials=/home/pi/.smbcredentials,uid=1000,gid=1000,iocharset=utf8 0 0"
+echo "//192.168.0.102/media/Videos /media/nas cifs credentials=/home/pi/.smbcredentials,uid=1000,gid=1000,iocharset=utf8 0 0"
