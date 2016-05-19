43f85d752d356d2d5db6dc6c745ba9c256d0cdf3
Dan Hughes <danhughes.dh+github@gmail.com>
2016-05-19 09:29:50 +0100

    Add Gist command, defaulting to private and auto-open

--- bash/includes/commands.sh
+++ bash/includes/commands.sh
@@ -3,3 +3,4 @@
 alias get='apt-get install'
 alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove -y && sudo apt-get clean'
 alias ssh-forget='ssh-keygen -f "~/.ssh/known_hosts" -R '
+alias gist='gist -op'
