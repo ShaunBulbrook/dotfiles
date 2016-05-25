1d23a98f1c7ff9cffdd6344720514c13e2409d40
Dan Hughes <danhughes.dh+github@gmail.com>
2016-05-25 10:19:07 +0100

    Add nano alias for Notepad++

--- bash/includes/commands.sh
+++ bash/includes/commands.sh
@@ -4,3 +4,11 @@ alias get='apt-get install'
 alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove -y && sudo apt-get clean'
 alias ssh-forget='ssh-keygen -f "~/.ssh/known_hosts" -R '
 alias gist='gist -op'
+
+nano() {
+	if [ "$#" -lt 1 ]; then
+		echo "Usage: $FUNCNAME <file>"
+	else
+		"/c/Program Files (x86)/Notepad++/notepad++.exe" "$1"
+	fi
+}
