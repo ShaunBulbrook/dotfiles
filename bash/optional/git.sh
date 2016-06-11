f5a712ef2e60f080e4f3603bceeaf97703a61456
Dan Hughes <danhughes.dh+github@gmail.com>
2016-06-11 17:28:54 +0100

    Add git diff command

--- bash/optional/git.sh
+++ bash/optional/git.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 alias gc='git commit -m'
+alias gd='git diff'
 alias gp='git push'
 alias ga='git add --all --verbose'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n 10"
