4553e995978d7cc763a511665c21dd84d317fe38
Dan Hughes <danhughes.dh+github@gmail.com>
2016-06-21 21:37:51 +0100

    Add docker composer alias

--- bash/optional/docker.sh
+++ bash/optional/docker.sh
@@ -1,3 +1,5 @@
 if [ "$(docker-machine status)" == "Running" ] ; then
 	eval $(docker-machine env default)
 fi
+
+alias composer='docker run --rm -v "/`pwd`:/app" -v "/`realpath ~/.ssh`:/root/.ssh" composer/composer $@'
