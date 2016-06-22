10d7e44810ba328bf9f6f20ee81b5a8b21ea9b9b
Dan Hughes <danhughes.dh+github@gmail.com>
2016-06-22 22:14:10 +0100

    Move nano command out to separate file

---
+++ bash/optional/windows.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+nano() {
+	if [ "$#" -lt 1 ]; then
+		echo "Usage: $FUNCNAME <file>"
+	else
+		("/c/Program Files (x86)/Notepad++/notepad++.exe" "$1" -notabbar -multiInst -nosession &)
+	fi
+}
