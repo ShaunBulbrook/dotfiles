43d852f53f6b4d1e6b5d1ccfa47eff5b978eb32d
Dan Hughes <danhughes.dh+github@gmail.com>
2016-07-07 16:14:59 +0100

    Show file tab bar in Notepad++

--- bash/optional/windows.sh
+++ bash/optional/windows.sh
@@ -4,6 +4,6 @@ nano() {
 	if [ "$#" -lt 1 ]; then
 		echo "Usage: $FUNCNAME <file>"
 	else
-		("/c/Program Files (x86)/Notepad++/notepad++.exe" "$1" -notabbar -multiInst -nosession &)
+		("/c/Program Files (x86)/Notepad++/notepad++.exe" "$1" -multiInst -nosession &)
 	fi
 }
