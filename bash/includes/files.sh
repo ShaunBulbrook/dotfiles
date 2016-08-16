fc10d9f79dbceed9bee56d12110853e7d3827ec5
Dan Hughes <danhughes.dh+github@gmail.com>
2016-08-16 10:40:20 +0100

    New function to get single line from file/stdin

--- bash/includes/files.sh
+++ bash/includes/files.sh
@@ -5,6 +5,10 @@ alias exify='chmod u+x'
 alias gpgify='gpg --encrypt --recipient $GPGKEY --trust-model always --armor'
 alias tmp='cd $(mktemp -d)'
 
+line() {
+	sed -n "$1p"
+}
+
 rewrite() {
 	echo "" > $1
 	nano $1
