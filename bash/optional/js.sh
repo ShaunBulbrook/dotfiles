b97a66d0abae7ba335226ff4014da249f04241aa
Dan Hughes <danhughes.dh+github@gmail.com>
2016-09-08 20:09:47 +0100

    Remove unused hg commands

--- bash/optional/js.sh
+++ bash/optional/js.sh
@@ -10,32 +10,3 @@ function beautify {
 		js-beautify --config "~/.jsbeautifyrc" -f "$file" -o "$file"
 	done
 }
-
-function hgbeautify {
-	dir=$(pwd)
-
-	command cd $(hg root) # Use inbuilt to prevent LS after CD
-	hg status --no-status | while read FILE; do
-		if [ -f "$FILE" ]; then
-			beautify $FILE
-		else
-			echo "Err: $FILE not found"
-		fi
-	done
-
-	command cd $dir
-}
-
-function hglint {
-	dir=$(pwd)
-
-	command cd $(hg root) # Use inbuilt to prevent LS after CD
-	hg status --no-status | while read FILE; do
-		if [ -f "$FILE" ] && [ ${FILE: -3} == ".js" ]; then
-			jshint "$FILE"
-		else
-			echo "Skipped $FILE"
-		fi
-	done
-	command cd $dir
-}
