889103f4fb716f9d99b1bfe0cf011b7ed99df078
Dan Hughes <danhughes.dh+github@gmail.com>
2016-09-20 19:29:08 +0100

    Remove file header from grep

--- bash/includes/colours.sh
+++ bash/includes/colours.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 alias ls='ls --color -F --group-directories-first'
-alias grep='grep -H --color=auto'
+alias grep='grep --color=auto'
 alias less='less -r'
 alias ccat='pygmentize'
