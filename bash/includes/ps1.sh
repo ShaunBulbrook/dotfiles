d2f60e605ba4829ff814ac034aa1ff0f085e05f9
Dan Hughes <danhughes.dh+github@gmail.com>
2016-12-02 13:56:15 +0000

    Fix colour escape sequences

--- bash/includes/ps1.sh
+++ bash/includes/ps1.sh
@@ -3,33 +3,28 @@
 # Enable the * in `__git_ps1` when uncommited files
 export GIT_PS1_SHOWDIRTYSTATE=1
 
-r="\e[0m"
-bold="\e[1m"
-dim="\e[2m"
+r="\[\e[0m\]"
+bold="\[\e[1m\]"
+dim="\[\e[2m\]"
 
-black="\e[30m"
-red="\e[31m"
-green="\e[32m"
-yellow="\e[33m"
-blue="\e[34m"
-magenta="\e[35m"
-cyan="\e[36m"
-lightgray="\e[37m"
-darkgray="\e[90m"
-lightred="\e[91m"
-lightgreen="\e[92m"
-lightyellow="\e[93m"
-lightblue="\e[94m"
-lightmagenta="\e[95m"
-lightcyan="\e[96m"
-white="\e[97m"
+black="\[\e[30m\]"
+red="\[\e[31m\]"
+green="\[\e[32m\]"
+yellow="\[\e[33m\]"
+blue="\[\e[34m\]"
+magenta="\[\e[35m\]"
+cyan="\[\e[36m\]"
+lightgray="\[\e[37m\]"
+darkgray="\[\e[90m\]"
+lightred="\[\e[91m\]"
+lightgreen="\[\e[92m\]"
+lightyellow="\[\e[93m\]"
+lightblue="\[\e[94m\]"
+lightmagenta="\[\e[95m\]"
+lightcyan="\[\e[96m\]"
+white="\[\e[97m\]"
 
-usrcol="${bold}${!USERNAMECOLOUR}"
-pwdcol="${!PWDCOLOUR}"
-
-PS1=''
-
-PS1+="\n${usrcol}\h${r} ${pwdcol}\w${r}"
+PS1="\n${bold}${!USERNAMECOLOUR}\h${r} ${!PWDCOLOUR}\w${r}"
 
 PS1+="${yellow}"
 PS1+='`__git_ps1 " %s"`'
