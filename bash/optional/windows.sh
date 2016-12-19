e90d0025c40d0cf30eca1d3dc5f65197e7226510
Dan Hughes <danhughes.dh+github@gmail.com>
2016-12-19 10:54:32 +0000

    Use 64bit Notepad++

--- bash/optional/windows.sh
+++ bash/optional/windows.sh
@@ -4,6 +4,6 @@ nano() {
 	if [ "$#" -lt 1 ]; then
 		echo "Usage: $FUNCNAME <file>"
 	else
-		("/c/Program Files (x86)/Notepad++/notepad++.exe" "$1" -multiInst -nosession &)
+		("/c/Program Files/Notepad++/notepad++.exe" "$1" -multiInst -nosession &)
 	fi
 }
