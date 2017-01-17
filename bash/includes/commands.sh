17dc3a27dc2620e6ee5bcc2d4997832e4885f57b
Dan Hughes <danhughes.dh+github@gmail.com>
2017-01-17 14:46:04 +0000

    Shorten ssh-forget command

--- bash/includes/commands.sh
+++ bash/includes/commands.sh
@@ -2,5 +2,5 @@
 
 alias get='apt-get install'
 alias updateall='sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove -y && sudo apt-get clean'
-alias ssh-forget='ssh-keygen -f "~/.ssh/known_hosts" -R '
+alias ssh-forget='ssh-keygen -R'
 alias gist='gist -op'
