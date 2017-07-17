63e4bc194247d256c9f3044aef922a7d41b18660
Dan Hughes <dan.hughes@heathwallace.com>
2017-07-17 10:45:45 +0100

    Delete obsolete docker-machine startup script

--- bash/optional/docker.sh
+++ bash/optional/docker.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$(docker-machine status)" == "Running" ] ; then
-	eval $(docker-machine env default)
-fi
-
 alias composer='docker run --rm -v "/`pwd`:/app" -v "/`realpath ~/.ssh`:/root/.ssh" composer/composer $@'
