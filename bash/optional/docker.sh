ea52c4b39d2fd3519b1188dbab4450ab2308bd30
Dan Hughes <dan.hughes@heathwallace.com>
2017-08-02 17:26:30 +0100

    Add jekyll alias for docker

--- bash/optional/docker.sh
+++ bash/optional/docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 alias composer='docker run --rm -v "/`pwd`:/app" -v "/`realpath ~/.ssh`:/root/.ssh" composer/composer $@'
+alias jekyll='docker run -v "/`pwd`:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll $@'
