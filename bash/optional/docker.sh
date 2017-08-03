f0ca99d610fd2f9687fa7bd836b747129a8622c9
Dan Hughes <dan.hughes@heathwallace.com>
2017-08-03 17:29:41 +0100

    Add "t" support to bash and powershell

--- bash/optional/docker.sh
+++ bash/optional/docker.sh
@@ -2,3 +2,4 @@
 
 alias composer='docker run --rm -v "/`pwd`:/app" -v "/`realpath ~/.ssh`:/root/.ssh" composer/composer $@'
 alias jekyll='docker run -v "/`pwd`:/srv/jekyll" -p 4000:4000 jekyll/jekyll jekyll $@'
+alias t='docker run -v /$HOME/.trc:/root/.trc dan1elhughes/docker-t $@'
--- installers/powershell/profile.ps1
+++ installers/powershell/profile.ps1
@@ -6,3 +6,8 @@ Set-Location $Home
 
 # Open to a blank prompt
 Clear-Host
+
+function t {
+	$h = $Home -replace "\\", "/" -replace ":", ""
+	docker run --rm -v //$h/.trc:/root/.trc dan1elhughes/docker-t @args
+}
