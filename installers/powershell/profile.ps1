fa4d9dc8e5ee3c161c11130e63ff4f82a6d46987
Dan Hughes <dan.hughes@heathwallace.com>
2017-08-04 10:48:15 +0100

    Add docker mongod alias

--- installers/powershell/profile.ps1
+++ installers/powershell/profile.ps1
@@ -11,3 +11,7 @@ function t {
 	$h = $Home -replace "\\", "/" -replace ":", ""
 	docker run --rm -v //$h/.trc:/root/.trc dan1elhughes/docker-t @args
 }
+
+function mongod {
+	docker run -d -p 27017:27017 -v mongodbdata:/data/db mongo
+}
