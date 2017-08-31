bc9e472f5ebd8964619bfe0a482bf02e0f2d8b2e
Dan Hughes <danhughes.dh+github@gmail.com>
2017-08-31 21:07:19 +0100

    Run t in interactive mode in docker

--- installers/powershell/profile.ps1
+++ installers/powershell/profile.ps1
@@ -9,7 +9,7 @@ Clear-Host
 
 function t {
 	$h = $Home -replace "\\", "/" -replace ":", ""
-	docker run --rm -v //$h/.trc:/root/.trc dan1elhughes/docker-t @args
+	docker run -it --rm -v //$h/.trc:/root/.trc dan1elhughes/docker-t @args
 }
 
 function mongod {
