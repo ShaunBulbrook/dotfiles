26bf491973fc7feba2e8132e15ef4a02a3021bc3
Dan Hughes <danhughes.dh+github@gmail.com>
2017-09-01 19:08:51 +0100

    Add rc.local installer into misc

---
+++ misc/rc.local/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#--------------------------------------------------------------------------
+# rc.local
+#--------------------------------------------------------------------------
+#
+# Sends boot notifications to a given Pushbullet API key
+#
+#
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <Pushbullet API key>"
+	exit 1
+fi
+
+KEY=$1
+
+cp rc.local rc.local.temp
+sed -i "s/XXAPIKEYXX/$KEY/g" rc.local.temp
+
+echo "Moving new rc.local to real location - password may be required"
+
+sudo mv rc.local.temp /etc/rc.local
+sudo chown root: /etc/rc.local
